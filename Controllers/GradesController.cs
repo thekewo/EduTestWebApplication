55ce7d121981a3757c5f0bc27f13b00b7fbdb5b6
Koós Zoltán <koos.zoltan.iso@gmail.com>
2021-06-15 16:38:30 +0200

    Added Grades controller, viewmodel. Added mapping between Grade model, viewmodel. Created basic pages for Grade.

---
+++ Controllers/GradesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduTestWebApplication.Data;
+using EduTestWebApplication.ViewModels;
+
+namespace EduTestWebApplication.Controllers
+{
+    public class GradesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GradesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Grades
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.GradeViewModel.ToListAsync());
+        }
+
+        // GET: Grades/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gradeViewModel = await _context.GradeViewModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gradeViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(gradeViewModel);
+        }
+
+        // GET: Grades/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Grades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,StudentId,Value")] GradeViewModel gradeViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                gradeViewModel.Id = Guid.NewGuid();
+                _context.Add(gradeViewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gradeViewModel);
+        }
+
+        // GET: Grades/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gradeViewModel = await _context.GradeViewModel.FindAsync(id);
+            if (gradeViewModel == null)
+            {
+                return NotFound();
+            }
+            return View(gradeViewModel);
+        }
+
+        // POST: Grades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,StudentId,Value")] GradeViewModel gradeViewModel)
+        {
+            if (id != gradeViewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gradeViewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GradeViewModelExists(gradeViewModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gradeViewModel);
+        }
+
+        // GET: Grades/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gradeViewModel = await _context.GradeViewModel
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (gradeViewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(gradeViewModel);
+        }
+
+        // POST: Grades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var gradeViewModel = await _context.GradeViewModel.FindAsync(id);
+            _context.GradeViewModel.Remove(gradeViewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GradeViewModelExists(Guid id)
+        {
+            return _context.GradeViewModel.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Profiles/GradesProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EduTestWebApplication.Models;
+using EduTestWebApplication.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduTestWebApplication.Profiles
+{
+    public class GradesProfile : Profile
+    {
+        public GradesProfile()
+        {
+            CreateMap<Grade, GradeViewModel>();
+            CreateMap<GradeViewModel, Student>();
+        }
+    }
+}
